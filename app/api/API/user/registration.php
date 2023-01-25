2526ea87589ef438a999f4d62ce1d1d48a94425c
Angelo Pavan <angelopavan.dev@gmail.com>
2023-01-25 08:43:40 +0100

    Aggiunte le API per il lato backend.

---
+++ app/api/API/user/registration.php
@@ -0,0 +1,25 @@
+<?php
+require __DIR__ . '/../../COMMON/connect.php';
+require __DIR__ . '/../../MODEL/user.php';
+header("Content-type: application/json; charset=UTF-8");
+
+$data = json_decode(file_get_contents("php://input"));
+
+if (empty($data->name) || empty($data->surname) || empty($data->email) || empty($data->password)) {
+    http_response_code(400);
+    echo json_encode(["message" => "Fill every field"]);
+    die();
+}
+
+$db = new Database();
+$db_conn = $db->connect();
+$user = new User($db_conn);
+$hash = $data->password; 
+$password = password_hash($hash, PASSWORD_BCRYPT);
+
+if ($user->registration($data->name, $data->surname, $data->email, $password) == true) {
+    echo json_encode(["message" => "1"]);
+} else {
+    echo json_encode(["message" => "-1"]);
+}
+?>
