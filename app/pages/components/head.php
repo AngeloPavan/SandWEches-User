1cedda961bec3c8e0ea7e0a7d2435fd39f769913
Angelo Pavan <angelopavan.dev@gmail.com>
2023-01-25 09:23:17 +0100

    Spostata la sezione head in un file.

---
+++ app/pages/components/head.php
@@ -0,0 +1,11 @@
+<meta charset="UTF-8">
+<meta http-equiv="X-UA-Compatible" content="IE=edge">
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+<!-- CDN tailwind css -->
+<script src="https://cdn.tailwindcss.com"></script>
+<script src="https://cdn.tailwindcss.com?plugins=forms,typography,aspect-ratio,line-clamp"></script>
+
+<!-- css -->
+<link rel="icon" type="image/x-icon" href="..\assets\img\logo.png">
+<!-- <link rel="stylesheet" href="../assets/style.css"> -->
