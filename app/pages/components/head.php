e20a24a2c9aed76926c15caf3d6878834ccb3fb6
Angelo Pavan <angelopavan.dev@gmail.com>
2023-01-25 13:22:44 +0100

    Aggiunto un file php con i contenuti dell'head.

--- app/pages/components/head.php
+++ app/pages/components/head.php
@@ -1,11 +1,15 @@
+<!--
+In questa pagina vanno aggiunti tutti gli script
+da includere dentro l'head
+-->
+
 <meta charset="UTF-8">
 <meta http-equiv="X-UA-Compatible" content="IE=edge">
 <meta name="viewport" content="width=device-width, initial-scale=1.0">
 
-<!-- CDN tailwind css -->
-<script src="https://cdn.tailwindcss.com"></script>
-<script src="https://cdn.tailwindcss.com?plugins=forms,typography,aspect-ratio,line-clamp"></script>
+<!-- CDN bootstrap css -->
+<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-GLhlTQ8iRABdZLl6O3oVMWSktQOp6b7In1Zl3/Jr59b6EGGoI1aFkw7cmDA6j6gD" crossorigin="anonymous">
 
 <!-- css -->
 <link rel="icon" type="image/x-icon" href="..\assets\img\logo.png">
-<!-- <link rel="stylesheet" href="../assets/style.css"> -->
+<link rel="stylesheet" href="..\assets\css\style.css">
