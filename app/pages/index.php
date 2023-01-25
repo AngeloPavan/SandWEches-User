5ae1d3095c72c4cafc292534d0754e48b94f0dc1
Angelo Pavan <angelopavan.dev@gmail.com>
2023-01-25 09:23:26 +0100

    Iniziata la pagina di login.

---
+++ app/pages/index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <?php require_once(__DIR__.'\components\head.php'); ?>
+
+    <title>SandWEches | Login</title>
+</head>
+
+<body>
+    <div class="container mx-auto">
+        <h1 class="prose-2xl">Hello, world.</h1>
+    </div>
+</body>
+
+</html>
