341a6cb5af9756771818bdf988467bb91feec591
Angelo Pavan <angelopavan.dev@gmail.com>
2023-01-26 16:35:00 +0100

    Iniziata l'home page.

---
+++ app/pages/homepage.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <?php require_once(__DIR__.'\components\head.php'); ?>
+
+    <title>SandWEches</title>
+</head>
+
+<body>
+    <div class="container">
+        <h1>Hello, world.</h1>
+    </div>
+
+    <?php require_once(__DIR__.'\components\script.php'); ?>
+</body>
+
+</html>
