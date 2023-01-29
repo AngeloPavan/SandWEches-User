bdb4fe464834c734290088c4124b102560768a24
Angelo Pavan <angelopavan.dev@gmail.com>
2023-01-29 10:45:33 +0100

    Aggiornata la pagina del recupero della password.
    Non è ancora funzionante.

--- app/pages/forgotPassword.php
+++ app/pages/forgotPassword.php
@@ -21,19 +21,19 @@
                     <h6 class="text-center">Se la tua email corrisponde ad un account esistente allora riceverai un messaggio di conferma da parte del nostro staff.</h6>
                 </div>
                 <!-- campo email -->
-                <div class="mb-3">
+                <div class="mb-5">
                     <label for="inputEmail" class="form-label">Indirizzo Email</label>
                     <input type="text" class="form-control" id="inputEmail" placeholder="mario.rossi@gmail.com">
                     <!-- <div id="emailHelp" class="form-text">Tranquillo, non venderemo i tuoi dati a nessuno.</div> -->
                 </div>
                 <!-- tasto per accedere -->
-                <div class="text-center">
-                    <button type="button" class="btn btn-warning px-5 fs-5 fw-bold">Invia</button>
-                    <!-- tasto password dimenticata -->
-                    <div class="mt-3">
-                        <a href="index.php">Accedi</a>
-                    </div>
-                </div>
+				<div class="text-center">
+					<button type="submit" class="btn btn-warning px-5 fs-5 fw-bold">Crea</button>
+					<!-- tasto password dimenticata -->
+					<div class="mt-3">
+						<a rolee="button" class="btn btn-success px-5 fs-5 fw-bold" href="index.php">Accedi</a>
+					</div>
+				</div>
             </form>
         </div>
     </div>
