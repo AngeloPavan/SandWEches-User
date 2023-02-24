cc2da9cc5d7987387cb0d43aac87eaab08b8efef
Angelo Pavan <angelopavan.dev@gmail.com>
2023-02-24 15:58:04 +0100

    Aggiunte le categorie principali nella homepage.

--- app/pages/homepage.php
+++ app/pages/homepage.php
@@ -12,13 +12,32 @@ $page = 'home';
 <html lang="en">
 
 <head>
-<?php require_once(__DIR__.'\components\head.php'); ?>
+    <?php require_once(__DIR__.'\components\head.php'); ?>
 
-<title>SandWEches</title>
+    <title>SandWEches</title>
 </head>
 
 <body>
     <?php require_once(__DIR__.'\components\navbar.php'); ?>
+
+    <div class="container-fluid">
+        <div class="row home-item-row">
+            <div class="col-7 home-item panino">
+                <h1 class="text-center">PANINI</h1>
+            </div>
+            <div class="col-4 home-item snack">
+                <h1 class="text-center">SNACKS</h1>
+            </div>
+        </div>
+        <div class="row home-item-row">
+            <div class="col-5 home-item bibita">
+                <h1 class="text-center">BEVANDE</h1>
+            </div>
+            <div class="col-6 home-item piadina">
+                <h1 class="text-center">PIADINE</h1>
+            </div>
+        </div>
+    </div>
 
     <?php require_once(__DIR__.'\components\script.php'); ?>
 </body>
